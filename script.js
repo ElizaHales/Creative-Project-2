6285e6bf929e5f2390009acc3331496b4c50825f
ElizaHales <59296568+ElizaHales@users.noreply.github.com>
2021-02-27 20:33:02 -0700

    rough draft

--- script.js
+++ script.js
@@ -1,58 +1,84 @@
-document.getElementById("weatherSubmit").addEventListener("click", function(event) {
-  event.preventDefault();
-  const value = document.getElementById("weatherInput").value;
-  if (value === "")
-    return;
-  console.log(value);
+// JavaScript for the collapsibles
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-  const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=abcbe3c9630266bd918577194a0d4318";
-  fetch(url)
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      let results = "";
-      results += "<hr style='height: 2px; color: gray; background-color: gray'><h2>Weather in " + json.name;
-      for (let i=0; i < json.weather.length; i++) {
-	results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-      }
-      results += '</h2>';
-      results += '<h3>Description: ';
-      for (let i=0; i < json.weather.length; i++) {
-        results += json.weather[i].description
-        if (i !== json.weather.length - 1)
-          results += ", "
-      }
-      results += '</h3>';
-      results += '<div class="row">';
-      results += '<p class="column">Temperature: ' + json.main.temp + ' &deg;F<br>';
-      results += 'Feels like: ' + json.main.feels_like + ' &deg;F</p>';
-      results += '<p class="column">Min: ' + json.main.temp_min + ' &deg;F<br>';
-      results += 'Max: ' + json.main.temp_max + ' &deg;F</p>';
-      results += '</div>';
-      document.getElementById("weatherResults").innerHTML = results;
-    });
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
-  const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=abcbe3c9630266bd918577194a0d4318";
-  fetch(url2)
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      let forecast = "";
-      for (let i=0; i < json.list.length; i++) {
-        if (i == 0) {
-	  forecast += "<hr style='height: 2px; color: gray; background-color: gray'><h2>" + moment(json.list[i].dt_txt).format('MMMM DD, YYYY') + "</h2>";
-	} else if (moment(json.list[i].dt_txt).format('ha') == "12am") {
-          forecast += "<hr style='height: 2px; color: gray; background-color: gray'><h2>" + moment(json.list[i].dt_txt).format('MMMM DD, YYYY') + "</h2>";
-        }
-        forecast += "<hr style='width: 95%'><div><h3>" + moment(json.list[i].dt_txt).format('ha') + '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>' + '</h3></div>';
-        forecast += '<div class="row">';
-        forecast += '<p class="column">Temperature: ' + json.list[i].main.temp + ' &deg;F<br>';
-        forecast += 'Feels like: ' + json.list[i].main.feels_like + ' &deg;F</p>';
-        forecast += '<p class="column">Min: ' + json.list[i].main.temp_min + ' &deg;F<br>';
-        forecast += 'Max: ' + json.list[i].main.temp_max + ' &deg;F</p>';
-        forecast += '</div><br>';
-      }
-      document.getElementById("forecastResults").innerHTML = forecast;
-    });
+// endpoint #1
+const url = "https://www.dnd5eapi.co/api/monsters/giant-frog";
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    let results = "";
+    results += "He would be a " + json.name + ", which are of type \"" + json.type;
+    results += ".\" What makes the " + json.name + " a " + json.type + "?";
+    results += " On a scale of 0 to 15, they have ";
+    results += "<ul><li>strength: " + json.strength + ",</li>";
+    results += "<li>dexterity: " + json.dexterity + ",</li>";
+    results += "<li>constitution: " + json.constitution + ",</li>";
+    results += "<li>and wisdom: " + json.wisdom + ".</li></ul>";
+    document.getElementById("monsters_giant-frog").innerHTML = results;
+  });
 
-});
+// endpoint #2
+const url2 = "https://www.dnd5eapi.co/api/skills/athletics";
+fetch(url2)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    let results = "";
+    results += "What would be his greatest skill? " + json.name + ".";
+    results += " Here is the description: \"" + json.desc + "\"";
+    document.getElementById("skills_athletics").innerHTML = results;
+  });
+
+// endpoint #3
+const url3 = "https://www.dnd5eapi.co/api/spells/heroism";
+fetch(url3)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    let results = "";
+    results += "What would be his favorite spell? " + json.name + ".";
+    results += " Here is the description: \"" + json.desc + "\"";
+    document.getElementById("spells_heroism").innerHTML = results;
+  });
+
+// endpoint #4
+const url4 = "https://www.dnd5eapi.co/api/traits/relentless-endurance";
+fetch(url4)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    let results = "";
+    results += "What would be one of his dominant traits? " + json.name + ".";
+    results += " Here is the description: \"" + json.desc + "\"";
+    document.getElementById("traits_relentless-endurance").innerHTML = results;
+  });
+
+// endpoint #5
+const url5 = "https://www.dnd5eapi.co/api/magic-items/carpet-of-flying";
+fetch(url5)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    let results = "";
+    results += "What would be his magic item of choice? " + json.name + ".";
+    results += " Here is the description:<ul>";
+    for (let i = 0; i < json.desc.length; i++) {
+      results += "<li>" + json.desc[i] + "</li>";
+    }
+    results += "</ul>"
+    document.getElementById("magic-items_carpet-of-flying").innerHTML = results;
+  });
